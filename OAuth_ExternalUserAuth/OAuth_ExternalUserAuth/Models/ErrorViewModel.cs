7f439a30de4d7fb9a55a14cf833c72d8c7fbcd96
zubairshakoorarbisoft <zubair.shakoor@arbisoft.com>
2019-12-10 19:16:37 +0500

    Facebook and Google OAuth added

---
+++ OAuth_ExternalUserAuth/OAuth_ExternalUserAuth/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OAuth_ExternalUserAuth.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
